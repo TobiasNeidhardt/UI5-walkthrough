8e8f324707a79268cae7a9e6dac256c928f5600a
Neidhardt <tobias.neidhardt@accenture.com>
2022-06-10 09:53:04 +0200

    Step 33: Custom Controls - Rating

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -12,6 +12,7 @@ sap.ui.define([
 		},
 
 		_onObjectMatched: function (oEvent) {
+			this.byId("rating").reset();
 			this.getView().bindElement({
 				path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").invoicePath),
 				model: "invoice"
@@ -28,6 +29,12 @@ sap.ui.define([
 				var oRouter = this.getOwnerComponent().getRouter();
 				oRouter.navTo("overview", {}, true);
 			}
+		},
+		onRatingChange: function (oEvent) {
+			var fValue = oEvent.getParameter("value");
+			var oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+
+			MessageToast.show(oResourceBundle.getText("ratingConfirmation", [fValue]));
 		}
 
 	});
